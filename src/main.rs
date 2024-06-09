2ae782d3fe174a39e3d60b149ca4ff3e468cdf74
luke <lukas_enderle@web.de>
2024-06-09 17:30:50 +0200

    gow-software project

---
+++ src/main.rs
@@ -0,0 +1,13 @@
+use std::time::Duration;
+use std::sync::Arc;
+use tokio::time;
+use tokio::sync::Mutex;
+use serialport::prelude::*;
+use std::io::Write;
+use chrono::{Utc, Duration as ChronoDuration};
+
+
+#[tokio::main]
+async fn main() {
+    
+}
